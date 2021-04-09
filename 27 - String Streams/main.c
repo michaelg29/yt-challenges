d6b05ea82f7840e419b83073e0882a62d51d0db9
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2021-04-09 16:06:23 -0400

    Added substring method

--- 27 - String Streams/main.c
+++ 27 - String Streams/main.c
@@ -18,11 +18,16 @@ int main()
     printf("{%d, %d, \"%s\"}\n", s.size, s.capacity, s.str);
 
     strstream_concat(&s, "My name is %s\t", "Facemaskdjlksahfalskjdfhlaksdjf");
-    printf("{%d, %d, \"%s\"}\n", s.size, s.capacity, s.str);
+    printf("{%d, %d, \"%s\"}\n\n", s.size, s.capacity, s.str);
 
     fp = fopen("test2.txt", "wb");
     strstream_writeFile(&s, fp, 0, 0);
     fclose(fp);
+
+    printf("%s\n", strstream_substrRange(&s, 0, 17));
+    printf("%s\n", strstream_substrRange(&s, 17, 0));
+    printf("%s\n", strstream_substrLength(&s, 0, 17));
+    printf("%s\n", strstream_substrLength(&s, 17, -17));
 
     strstream_clear(&s);
 
