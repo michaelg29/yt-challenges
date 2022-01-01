2f46d4880f89bda5a27f1bac7375b32810233a2c
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-01-01 18:32:35 -0500

    Update strstream main.c

--- 27 - String Streams/main.c
+++ 27 - String Streams/main.c
@@ -5,6 +5,7 @@ int main()
 {
     printf("Hello, world!\n");
 
+    // concatenation and file io test
     strstream s = strstream_allocDefault();
     printf("{%d, %d, \"%s\"}\n", s.size, s.capacity, s.str);
 
@@ -28,6 +29,22 @@ int main()
     printf("%s\n", strstream_substrRange(&s, 17, 0));
     printf("%s\n", strstream_substrLength(&s, 0, 17));
     printf("%s\n", strstream_substrLength(&s, 17, -17));
+
+    strstream_clear(&s);
+
+    // split string test
+    s = strstream_fromStr("hello this is a string with spaces");
+
+    char **list;
+    int n = strstream_split(&s, ' ', &list);
+
+    printf("%d strings\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%s\n", list[i]);
+    }
+
+    freeStringList(list, n);
 
     strstream_clear(&s);
 
