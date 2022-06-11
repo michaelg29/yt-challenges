3b2ca292568ca093918f34d6cd2f0830446523c6
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-06-10 22:19:45 -0400

    Create mime type reference file

---
+++ 32 - Static File Server/mime.cs
@@ -0,0 +1,80 @@
+	private static readonly IDictionary<string, string> mimeTypes = new Dictionary<string, string> 
+        {
+            { ".asf", "video/x-ms-asf" },
+            { ".asx", "video/x-ms-asf" },
+            { ".avi", "video/x-msvideo" },
+            { ".bin", "application/octet-stream" },
+            { ".cco", "application/x-cocoa" },
+            { ".crt", "application/x-x509-ca-cert" },
+            { ".css", "text/css" },
+            { ".deb", "application/octet-stream" },
+            { ".der", "application/x-x509-ca-cert" },
+            { ".dll", "application/octet-stream" },
+            { ".dmg", "application/octet-stream" },
+            { ".ear", "application/java-archive" },
+            { ".eot", "application/octet-stream" },
+            { ".exe", "application/octet-stream" },
+            { ".flv", "video/x-flv" },
+            { ".gif", "image/gif" },
+            { ".hqx", "application/mac-binhex40" },
+            { ".htc", "text/x-component" },
+            { ".htm", "text/html" },
+            { ".html", "text/html" },
+            { ".ico", "image/x-icon" },
+            { ".img", "application/octet-stream" },
+            { ".iso", "application/octet-stream" },
+            { ".jar", "application/java-archive" },
+            { ".jardiff", "application/x-java-archive-diff" },
+            { ".jng", "image/x-jng" },
+            { ".jnlp", "application/x-java-jnlp-file" },
+            { ".jpeg", "image/jpeg" },
+            { ".jpg", "image/jpeg" },
+            { ".js", "application/x-javascript" },
+            { ".mml", "text/mathml" },
+            { ".mng", "video/x-mng" },
+            { ".mov", "video/quicktime" },
+            { ".mp3", "audio/mpeg" },
+            { ".mpeg", "video/mpeg" },
+            { ".mpg", "video/mpeg" },
+            { ".msi", "application/octet-stream" },
+            { ".msm", "application/octet-stream" },
+            { ".msp", "application/octet-stream" },
+            { ".pdb", "application/x-pilot" },
+            { ".pdf", "application/pdf" },
+            { ".pem", "application/x-x509-ca-cert" },
+            { ".pl", "application/x-perl" },
+            { ".pm", "application/x-perl" },
+            { ".png", "image/png" },
+            { ".prc", "application/x-pilot" },
+            { ".ra", "audio/x-realaudio" },
+            { ".rar", "application/x-rar-compressed" },
+            { ".rpm", "application/x-redhat-package-manager" },
+            { ".rss", "text/xml" },
+            { ".run", "application/x-makeself" },
+            { ".sea", "application/x-sea" },
+            { ".shtml", "text/html" },
+            { ".sit", "application/x-stuffit" },
+            { ".swf", "application/x-shockwave-flash" },
+            { ".tcl", "application/x-tcl" },
+            { ".tk", "application/x-tcl" },
+            { ".txt", "text/plain" },
+            { ".war", "application/java-archive" },
+            { ".wasm", "application/wasm" },
+            { ".wbmp", "image/vnd.wap.wbmp" },
+            { ".wmv", "video/x-ms-wmv" },
+            { ".xml", "text/xml" },
+            { ".xpi", "application/x-xpinstall" },
+            { ".zip", "application/zip" }
+        };
+
+        public static string GetMimeType(string route)
+        {
+            // find extension
+            string ext = Path.GetExtension(route);
+
+            if (mimeTypes.ContainsKey(ext))
+            {
+                return mimeTypes[ext];
+            }
+            return "application/octet-stream";
+        }
